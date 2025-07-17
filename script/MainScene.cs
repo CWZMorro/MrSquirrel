ba6fc27892d1b2a00b48e9d789ca3720d0827ec8
Jieh Er <jieherkow@gmail.com>
2025-07-17 17:49:04 +0800

    - changed spelling error in script

--- script/MainScene.cs
+++ script/MainScene.cs
@@ -12,7 +12,7 @@ public partial class MainScene : Node2D
 		"Snake: The outcome is Mr Owl crashing out and then after Ms Beverly begs for him to tell the truth he apologizes for accusing " +
 		"Mr Snake. The players tells him that smart people make hard decisions and have to face truths that may hurt themselves. ",
 		"Beverly: Ya'll just wanted to know what really happens to Mr Squirrel. We're sorry for taking him away from you.",
-		"Snake: Mr Monkey, we may not have been good friends, but I assure you Mr Squirrel wouold be pleased knowing you spoke the truth."
+		"Snake: Mr Monkey, we may not have been good friends, but I assure you Mr Squirrel would be pleased knowing you spoke the truth."
 	};
 
 	// Called when the node enters the scene tree for the first time.
